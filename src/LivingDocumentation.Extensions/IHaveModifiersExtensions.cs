b280e5a192078d3deb5c877dfb0ce6dcf1ffc5f0
Michaël Hompus <michael@hompus.nl>
2022-02-14 16:49:56 +0100

    Add additional modifier check methods + tests

--- src/LivingDocumentation.Extensions/IHaveModifiersExtensions.cs
+++ src/LivingDocumentation.Extensions/IHaveModifiersExtensions.cs
@@ -46,4 +46,39 @@ public static class IHaveModifiersExtensions
     {
         return (iHaveModifiers.Modifiers & Modifier.Readonly) == Modifier.Readonly;
     }
+
+    public static bool IsConst(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Const) == Modifier.Const;
+    }
+
+    public static bool IsPartial(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Partial) == Modifier.Partial;
+    }
+
+    public static bool IsExtern(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Extern) == Modifier.Extern;
+    }
+
+    public static bool IsNew(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.New) == Modifier.New;
+    }
+
+    public static bool IsSealed(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Sealed) == Modifier.Sealed;
+    }
+
+    public static bool IsUnsafe(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Unsafe) == Modifier.Unsafe;
+    }
+
+    public static bool IsVirtual(this IHaveModifiers iHaveModifiers)
+    {
+        return (iHaveModifiers.Modifiers & Modifier.Virtual) == Modifier.Virtual;
+    }
 }
